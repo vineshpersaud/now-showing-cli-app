31a8919ad06363827f4bcd09949628809e0f7c27
codeesh <vineshpersaud@gmail.com>
2017-10-18 20:31:25 -0400

    first commit

---
+++ lib/now/showing.rb
@@ -0,0 +1,7 @@
+require "now/showing/version"
+
+module Now
+  module Showing
+    # Your code goes here...
+  end
+end
---
+++ lib/now/showing/version.rb
@@ -0,0 +1,5 @@
+module Now
+  module Showing
+    VERSION = "0.1.0"
+  end
+end
