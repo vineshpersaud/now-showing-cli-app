e64af03802df66cdb7402e71515225939311d8c1
codeesh <vineshpersaud@gmail.com>
2017-10-18 21:52:21 -0400

    add main menu for input

--- lib/now_showing/cli.rb
+++ lib/now_showing/cli.rb
@@ -2,6 +2,7 @@ class NowShowing::CLI
 
   def call
     welcome
+    menu
   end
 
   def welcome
@@ -11,6 +12,20 @@ class NowShowing::CLI
     puts "1. Top ten movies in theathers now"
     puts "2. Movings opening this week"
     puts ""
+  end
+
+  def menu
+    input = nil
+      print "Please enter the number for the option you would like to see or exit to quit gem." 
+      input = gets.strip
+      if input == "1"
+         top
+      elsif input == "2"
+      elsif input == "exit"
+      else
+        print "Please enter a valid option of 1/2 or exit. "
+        menu
+      end
   end
 
 end
